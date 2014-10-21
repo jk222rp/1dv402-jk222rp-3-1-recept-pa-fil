959f4ea1fcc046d7f4dd051579001df853ed10a4
jk222rp <jk222rp@student.lnu.se>
2014-10-22 00:53:14 +0200

    Klar

--- FiledRecipes/FiledRecipes.Domain/RecipeRepository.cs
+++ FiledRecipes/FiledRecipes.Domain/RecipeRepository.cs
@@ -127,5 +127,162 @@ namespace FiledRecipes.Domain
                 handler(this, e);
             }
         }
+
+
+
+
+
+//Komplettering, med metoderna som saknas!
+
+        //Läser in recept
+        public void Load()
+        {
+            try
+            {
+                //Skapar en lista som kan innehålla referenser till receptobjekt
+                List<IRecipe> recipes = new List<IRecipe>();
+
+                //Öppnar textfilen
+                using (StreamReader reader = new StreamReader(_path))
+                {
+                    //Variabler
+                    string line;
+                    RecipeReadStatus status = RecipeReadStatus.Indefinite;
+                    Recipe theRecipe = null;
+
+                    //Läser raderna från textfilen tills det är slut på filen
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        //Om det är en tom rad läses nästa rad in utan att gå igenom andra if:s
+                        if (line != "")
+                        {
+                            if (line == SectionRecipe) //Om det är en avdelning för nytt recept
+                            {
+                                //Sätter status till att nästa rad som läses in kommer att vara receptets namn
+                                status = RecipeReadStatus.New;
+                            } else if (line == SectionIngredients) { //Om det är avdelningen för ingredienser
+                                //Sätter status till att kommande rader som läses in kommer att vara receptets ingredienser
+                                status = RecipeReadStatus.Ingredient;
+                            } else if (line == SectionInstructions) { //Om det är avdelningen för instruktioner
+                                //Sätter status till att kommande rader som läses in kommer att vara receptets instruktioner
+                                status = RecipeReadStatus.Instruction;
+                            } else { //Annars är det ett namn, en ingrediens eller en instruktion
+                                //Kollar vad raden ska tolkas som
+                                switch (status)
+                                {
+                                    case RecipeReadStatus.New: //Om status är satt att raden ska tolkas som ett recepts namn
+
+                                        //Lägger föregående recept i listan med recept, om inte receptobjektet är tomt
+                                        if (theRecipe != null)
+                                        {
+                                            //Lägger till föregående recept till listan med recept innan receptobjektet skrivs över
+                                            recipes.Add(theRecipe);
+                                        }
+
+                                        //"Skapar nytt" receptobjekt med receptets namn
+                                        theRecipe = new Recipe(line);
+
+                                        break;
+
+                                    case RecipeReadStatus.Ingredient: //Om status är att raden ska tolkas som en ingrediens
+                                        
+                                        //Delar upp raden i delar
+                                        string[] values = line.Split(new char[] { ';' });
+
+                                        //Kastar undantag om antalet delar inte är tre i raden med ingrediensen
+                                        if (values.Length != 3)
+                                        {
+                                            throw new FileFormatException("Antalet delar i raden med ingrediensen är inte tre");
+                                        }
+
+                                        //Skapar ett ingrediensobjekt och initierar det med de tre delarna för mängd, mått och namn
+                                        Ingredient ingredient = new Ingredient();
+                                        ingredient.Amount = values[0];
+                                        ingredient.Measure = values[1];
+                                        ingredient.Name = values[2];
+
+                                        //Lägger till ingrediensen till receptets lista med ingredienser
+                                        theRecipe.Add(ingredient);
+
+                                        break;
+
+                                    case RecipeReadStatus.Instruction: //Om status är satt att raden ska tolkas som en instruktion
+
+                                        //Lägger till raden till receptets lista med instruktioner
+                                        theRecipe.Add(line);
+
+                                        break;
+
+                                    default: //Kastar undantag om inget av ovanstående fall stämmer
+                                        throw new FileFormatException("Något är fel!");
+
+                                }
+                            }
+                        }
+                    }
+                    //Lägger till det sista receptet i listan med recept
+                    recipes.Add(theRecipe);
+                }
+
+                //Tar bort tomma platser i listan med recept
+                recipes.TrimExcess();
+
+                //Sorterar listan med recept med avseende på receptens namn
+                IEnumerable<IRecipe> sortedRecipes = recipes.OrderBy(recipe => recipe.Name);
+                
+                //Tilldelar _recipes en referens till den sorterade listan
+                _recipes = new List<IRecipe>(sortedRecipes);
+
+                //Indikerar att listan med recept är oförändrad
+                IsModified = false;
+
+                //Utlöser händelse om att recept har lästs in
+                OnRecipesChanged(EventArgs.Empty);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //Öppnar en textfil och skriver recepten rad för rad till textfilen
+        public void Save()
+        {
+            try
+            {
+                //Öppnar textfilen som recepten ska skrivas till
+                using (StreamWriter writer = new StreamWriter(_path))
+                {
+                    //För varje recept i listan med recept
+                    foreach (Recipe recipe in _recipes)
+                    {
+                        //Skriver rad med formatering för avdelningen med recept och sedan receptets namn
+                        writer.WriteLine(SectionRecipe);
+                        writer.WriteLine(recipe.Name);
+
+                        //Skriver rad med formatering för avdelningen med ingredienser
+                        writer.WriteLine(SectionIngredients);
+                        //Skriver ut varje ingrediens i receptet med formatering för ingredienserna
+                        foreach (Ingredient ingredient in recipe.Ingredients)
+                        {
+                            writer.WriteLine("{0};{1};{2}", ingredient.Amount, ingredient.Measure, ingredient.Name);
+                        }
+
+                        //Skriver rad med formatering för avdelningen med instruktioner
+                        writer.WriteLine(SectionInstructions);
+                        //Skriver ut varje instruktion i receptet
+                        foreach (string instruction in recipe.Instructions)
+                        {
+                            writer.WriteLine(instruction);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -12,5 +12,42 @@ namespace FiledRecipes.Views
     /// </summary>
     public class RecipeView : ViewBase, IRecipeView
     {
+        //Visar ett recept
+        public void Show(IRecipe recipe)
+        {
+            //Visar receptets header med receptets namn
+            Header = recipe.Name;
+            ShowHeaderPanel();
+
+            //Skriver ut receptets ingredienser, rad efter rad
+            Console.WriteLine("\nIngredienser\n============");
+            foreach (Ingredient ingredient in recipe.Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            //Skriver ut receptets instruktioner, instruktion efter instruktion
+            Console.WriteLine("\nGör såhär\n=========");
+            int i = 1; //Används för numrering av instruktionerna
+            foreach (string instruction in recipe.Instructions)
+            {
+                //Sktriver ut formaterade rader med instruktionens nummer på en rad och sedan själva instruktionen
+                Console.WriteLine("<{0}>\n{1}\n", i, instruction);
+                ++i;
+            }
+        }
+
+        //Visar alla recept
+        public void Show(IEnumerable<IRecipe> recipes)
+        {
+            //För varje recept
+            foreach (Recipe recipe in recipes)
+            {
+                //Visar receptet
+                Show(recipe);
+
+                ContinueOnKeyPressed();
+            }
+        }
     }
 }
